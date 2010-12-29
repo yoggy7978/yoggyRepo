f425f90845ae032bcb2d5928d033c9770c1f2078
yoggy <yoggy.nours@gmail.com>
2010-12-29 17:59:13 +0100

    gogogo

--- android/YogSMS/YogSMS/src/local/yogsms/ContactList.java
+++ android/YogSMS/YogSMS/src/local/yogsms/ContactList.java
@@ -2,13 +2,11 @@ package local.yogsms;
 
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Dictionary;
 import java.util.HashMap;
 
 import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.ContentUris;
-import android.content.Context;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -17,13 +15,7 @@ import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.provider.ContactsContract.PhoneLookup;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class ContactList extends Activity {
@@ -42,11 +34,13 @@ public class ContactList extends Activity {
 
 			mContactList = (ListView) findViewById(R.id.contactList);
 
-			this.populateContactList();
+			ContactCursorAdapter adapter = new ContactCursorAdapter(
+					this, 
+					getSMSContacts());
+			mContactList.setAdapter(adapter);
 
 		} catch (Exception e) {
-			Toast toast = Toast.makeText(getApplicationContext(), e
-					.getMessage(), Toast.LENGTH_LONG);
+			Toast toast = Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG);
 			toast.show();
 			Log.e(TAG, "exception:", e);
 		}
@@ -64,71 +58,21 @@ public class ContactList extends Activity {
 		return BitmapFactory.decodeStream(input);
 	}
 
-	/**
-	 * Populate the contact list based on account currently selected in the
-	 * account spinner.
-	 */
-	private void populateContactList() {
-		ContactCursorAdapter adapter = new ContactCursorAdapter(
-				this, 
-				getContacts());
-		mContactList.setAdapter(adapter);
-
-	}
-
-	/**
-	 * Obtains the contact list for the currently selected account.
-	 * 
-	 * @return A cursor for for accessing the contact list.
-	 */
-	private Cursor getContacts() {
-		Uri uri = ContactsContract.Contacts.CONTENT_URI;
-		String[] projection = new String[] { ContactsContract.Contacts._ID, ContactsContract.Contacts.DISPLAY_NAME };
-		String selection = null;
-		String[] selectionArgs = null;
-		String sortOrder = ContactsContract.Contacts.DISPLAY_NAME + " COLLATE LOCALIZED ASC";
-
-		return managedQuery(uri, projection, selection, selectionArgs, sortOrder);
-	}	
-}
-
-class ContactCursorAdapter extends BaseAdapter {
-
-	public static final String TAG = "YogSMS";
-	Cursor mContacts;
-	Cursor mSMS;
-	Context mContext;
-	int mLayout;
-	LayoutInflater mInflater;
-	java.util.HashMap<String, Integer> map = new HashMap<String, Integer>();
-		
-	ArrayList<SMSContact> smsSenders;
-	
-	public ContactCursorAdapter(Context context, Cursor contacts) {
-		super();
-		mContext = context;
-		mContacts = contacts;
-		mLayout = R.layout.contact_entry;
-		mInflater = LayoutInflater.from(mContext);
-		
+	private ArrayList<SMSContact> getSMSContacts()
+	{
+		ArrayList<SMSContact> smscontacts = new ArrayList<SMSContact>();
 		Cursor sms = querySMS(null, null, null, null);
 		
-		/*int collcount = sms.getColumnCount();
-		
-		sms.moveToFirst();
-		for(int i = 0 ; i < collcount ; i++)
-		{
-			Log.d(TAG, sms.getColumnName(i) + " ;");
-		}*/
+		java.util.HashMap<String, SMSContact> map = new HashMap<String, SMSContact>();
 		int c = sms.getCount() ;
 		Log.d(TAG, "Nb SMS: " + c);
 		for(int i = 0 ; i < c ; i++)
 		{
-			sms.moveToPosition(i);
-			Log.d(TAG, "--> " + i);
-			String pers = sms.getString(sms.getColumnIndex("person"));
-			Log.d(TAG, "    person " + pers);
-			String address = sms.getString(sms.getColumnIndex("address"));
+			sms.moveToPosition(i); 
+
+		/*	Log.d(TAG, "--> " + i);
+				Log.d(TAG, "    person " + sms.getString(sms.getColumnIndex("person")));
+			Log.d(TAG, "    address " + sms.getString(sms.getColumnIndex("address")));
 			Log.d(TAG, "    thread_id " + sms.getString(sms.getColumnIndex("thread_id")));
 			Log.d(TAG, "    date " + sms.getString(sms.getColumnIndex("date")));
 			Log.d(TAG, "    protocol " + sms.getString(sms.getColumnIndex("protocol")));
@@ -139,19 +83,77 @@ class ContactCursorAdapter extends BaseAdapter {
 			Log.d(TAG, "    subject " + sms.getString(sms.getColumnIndex("subject")));
 			Log.d(TAG, "    body " + sms.getString(sms.getColumnIndex("body")));
 			Log.d(TAG, "    service_center " + sms.getString(sms.getColumnIndex("service_center")));
-			Log.d(TAG, "    locked " + sms.getString(sms.getColumnIndex("locked")));
-/*			SMSContact cont ;
-			if(!map.containsKey(person))
+			Log.d(TAG, "    locked " + sms.getString(sms.getColumnIndex("locked")));	*/			
+			
+			String address = sms.getString(sms.getColumnIndex("address"));
+			Uri uri = Uri.withAppendedPath(PhoneLookup.CONTENT_FILTER_URI, Uri.encode(address));
+			Cursor lookup = managedQuery(uri, null,null,null, null);
+			int id = 0 ;
+			if(lookup.getCount() > 0)
+			{
+				lookup.moveToFirst();
+				id = lookup.getInt(lookup.getColumnIndex(PhoneLookup._ID));
+				address = lookup.getString(lookup.getColumnIndex(PhoneLookup.NUMBER));
+			}
+			
+			SMSContact cont ;
+			if(!map.containsKey(address))
 			{
 				cont = new SMSContact();
+				smscontacts.add(cont);
+				map.put(address, cont);
 			}
 			else
 			{
-				cont = map.get(key)
-			}*/
+				cont = (SMSContact)map.get(address);
+			}
+			cont.contactID = id;
+			cont.smscount += 1;
+			// = sms.getInt(sms.getColumnIndex("person"));
+			cont.telephone = address;
+			cont.hasphoto = false;
+			if(lookup.getCount() > 0)
+			{
+				if(lookup.getString(lookup.getColumnIndex(PhoneLookup.PHOTO_ID)) != null)
+				{
+					cont.hasphoto = true;
+				}
+				cont.knownContact = true;
+				cont.displayName = lookup.getString(lookup.getColumnIndex(PhoneLookup.DISPLAY_NAME));
+			}
+			else
+			{
+				cont.knownContact = false;
+				cont.displayName = cont.telephone;
+			}
+				
+			if(!sms.getString(sms.getColumnIndex("read")).equals("1"))
+			{
+				cont.unread ++;
+			}
 		}
+		return smscontacts;
 	}
-/* SMS table
+	/*
+	private Cursor getContactFromId(int id) {
+		try {
+			Uri uri = ContentUris.withAppendedId(ContactsContract.Contacts.CONTENT_URI, id);
+			return managedQuery(uri, null, null, null, null);
+		}catch (Exception e) {
+			Toast toast = Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT);
+			toast.show();
+			Log.e(TAG, "exception:", e);
+		}		
+		return null;
+		
+	}		*/
+	
+	private Cursor querySMS(String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+		Uri uri = Uri.parse("content://sms");
+		return managedQuery(uri, null, null, null, null);
+	}			
+	
+	/* SMS table
 	_id 
 	thread_id 
 	address 
@@ -167,55 +169,16 @@ class ContactCursorAdapter extends BaseAdapter {
 	service_center 
 	locked */
 	
+	/*int collcount = sms.getColumnCount();
 	
-	private Cursor querySMS(String[] projection, String selection, String[] selectionArgs, String sortOrder) {
-		Uri uri = Uri.parse("content://sms");
-		return ((Activity) mContext).managedQuery(uri, null, null, null, null);
-	}	
-	
-	public int getCount() {
-		return mContacts.getCount();
-	}
-
-	public Object getItem(int arg0) {
-		mContacts.moveToPosition(arg0);
-		return mContacts.getString(mContacts.getColumnIndex(ContactsContract.Data.DISPLAY_NAME));
-	}
-
-	public long getItemId(int arg0) {
-		return arg0;
-	}
-
-	public View getView(int position, View convertView, ViewGroup parent) {
-
-		mContacts.moveToPosition(position);
-		if (convertView == null) {
-			convertView = mInflater.inflate(mLayout, null);
-		} else {
-			convertView = (View) convertView;
-		}
-		TextView tv = (TextView) convertView.findViewById(R.id.contact_displayname);
-		tv.setText(mContacts.getString(mContacts.getColumnIndex(ContactsContract.Data.DISPLAY_NAME)));
-		ImageView iv = (ImageView) convertView.findViewById(R.id.contact_photo);
-		Bitmap bm = ContactList.loadContactPhoto(mContext.getContentResolver(), mContacts.getInt(mContacts.getColumnIndex(PhoneLookup._ID)));
-		if (bm != null)
-			iv.setImageBitmap(bm);
-		
-		return convertView;
-	}
-
-}
-
-class SMSContact
-{
-	public boolean knownContact ;
-	public String displayName;
-	public String telephone;
-	public int contactID;
-	public int unread;
-	SMSContact()
+	sms.moveToFirst();
+	for(int i = 0 ; i < collcount ; i++)
 	{
-		unread = 0;
-	}
+		Log.d(TAG, sms.getColumnName(i) + " ;");
+	}*/
+	
+
+
+
 
 }
