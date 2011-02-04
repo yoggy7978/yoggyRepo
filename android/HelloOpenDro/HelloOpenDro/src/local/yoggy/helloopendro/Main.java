41fc3454aeacf229c01643edf867a60d58ab3ed8
yoggy <yoggy.nours@gmail.com>
2011-02-04 17:27:19 +0100

    HelloOpenDro

---
+++ android/HelloOpenDro/HelloOpenDro/src/local/yoggy/helloopendro/Main.java
@@ -0,0 +1,16 @@
+package local.yoggy.helloopendro;
+
+import android.app.Activity;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+
+public class Main extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState);
+ 		GLSurfaceView view = new GLSurfaceView(this);
+   		view.setRenderer(new OpenGLRenderer());
+   		setContentView(view);
+    }
+}
